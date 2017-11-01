5e7e507cebd472aede4ab7b677e352ff4ebfe35c
kiwoo <kiwoo.shin@berkeley.edu>
2017-11-01 00:57:56 -0700

    modify main, add model and mode argument

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from misc_util import set_global_seeds
 import tf_util as U
-# import benchmarks
 import os.path as osp
 import sys
 from misc_util import set_global_seeds, read_dataset, get_cur_dir, header
@@ -20,6 +19,9 @@ from data_manager import DataManager
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--model') # chairs, celeb, dsprites
+    parser.add_argument('--mode') # training, testing
+    
     parser.add_argument('--disentangled_feat', type=int)
     parser.add_argument('--chkfiles')
     parser.add_argument('--logfiles')
@@ -41,6 +43,10 @@ def main():
     cur_dir = get_cur_dir()
     img_dir = osp.join(cur_dir, dir_name)
     header("Load model")
+
+    model = args.model
+    mode = args.mode
+
     disentangled_feat = args.disentangled_feat
     chkfile_name = args.chkfiles
     logfile_name = args.logfiles
@@ -48,8 +54,17 @@ def main():
 
     latent_dim = 10
     entangled_feat = latent_dim - disentangled_feat
+
+    if model == 'chairs':
+        mynet = models.mymodel(name="mynet", img_shape = [64, 64, 1], latent_dim = latent_dim, disentangled_feat = disentangled_feat)
+    elif model == 'celeb':
+        mynet = models_celeba.mymodel(name="mynet", img_shape = [64, 64, 3], latent_dim = latent_dim, disentangled_feat = disentangled_feat)
+    elif model == 'dsprites':
+        mynet = models_2dshapes.mymodel(name="mynet", img_shape = [64, 64, 1], latent_dim = latent_dim, disentangled_feat = disentangled_feat)
+
+
     # mynet = mymodel_curr(name="mynet", img_shape = [64, 64, 1], latent_dim = 32)
-    mynet = mymodel(name="mynet", img_shape = [64, 64, 1], latent_dim = latent_dim, disentangled_feat = disentangled_feat)
+    # mynet = mymodel(name="mynet", img_shape = [64, 64, 1], latent_dim = latent_dim, disentangled_feat = disentangled_feat)
     # train_curr_net(model = mynet, img_dir = img_dir)
     # train_net(model = mynet, img_dir = img_dir, chkfile_name = chkfile_name, logfile_name = logfile_name, validatefile_name = validatefile_name, entangled_feat = entangled_feat)
     train_net(model = mynet, manager = manager, chkfile_name = chkfile_name, logfile_name = logfile_name, validatefile_name = validatefile_name, entangled_feat = entangled_feat)
