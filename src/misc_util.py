3dddaf1acd8084d7e9fc6425e427c846460b283a
kiwoo <kiwoo.shin@berkeley.edu>
2017-11-05 20:52:35 -0800

    remove gym library

--- src/misc_util.py
+++ src/misc_util.py
@@ -1,4 +1,3 @@
-import gym
 import numpy as np
 import os
 import pickle
@@ -245,152 +244,152 @@ def pretty_eta(seconds_left):
     return 'less than a minute'
 
 
-class RunningAvg(object):
-    def __init__(self, gamma, init_value=None):
-        """Keep a running estimate of a quantity. This is a bit like mean
-        but more sensitive to recent changes.
+# class RunningAvg(object):
+#     def __init__(self, gamma, init_value=None):
+#         """Keep a running estimate of a quantity. This is a bit like mean
+#         but more sensitive to recent changes.
 
-        Parameters
-        ----------
-        gamma: float
-            Must be between 0 and 1, where 0 is the most sensitive to recent
-            changes.
-        init_value: float or None
-            Initial value of the estimate. If None, it will be set on the first update.
-        """
-        self._value = init_value
-        self._gamma = gamma
+#         Parameters
+#         ----------
+#         gamma: float
+#             Must be between 0 and 1, where 0 is the most sensitive to recent
+#             changes.
+#         init_value: float or None
+#             Initial value of the estimate. If None, it will be set on the first update.
+#         """
+#         self._value = init_value
+#         self._gamma = gamma
 
-    def update(self, new_val):
-        """Update the estimate.
+#     def update(self, new_val):
+#         """Update the estimate.
 
-        Parameters
-        ----------
-        new_val: float
-            new observated value of estimated quantity.
-        """
-        if self._value is None:
-            self._value = new_val
-        else:
-            self._value = self._gamma * self._value + (1.0 - self._gamma) * new_val
+#         Parameters
+#         ----------
+#         new_val: float
+#             new observated value of estimated quantity.
+#         """
+#         if self._value is None:
+#             self._value = new_val
+#         else:
+#             self._value = self._gamma * self._value + (1.0 - self._gamma) * new_val
 
-    def __float__(self):
-        """Get the current estimate"""
-        return self._value
-
-
-class SimpleMonitor(gym.Wrapper):
-    def __init__(self, env):
-        """Adds two qunatities to info returned by every step:
-
-            num_steps: int
-                Number of steps takes so far
-            rewards: [float]
-                All the cumulative rewards for the episodes completed so far.
-        """
-        super().__init__(env)
-        # current episode state
-        self._current_reward = None
-        self._num_steps = None
-        # temporary monitor state that we do not save
-        self._time_offset = None
-        self._total_steps = None
-        # monitor state
-        self._episode_rewards = []
-        self._episode_lengths = []
-        self._episode_end_times = []
-
-    def _reset(self):
-        obs = self.env.reset()
-        # recompute temporary state if needed
-        if self._time_offset is None:
-            self._time_offset = time.time()
-            if len(self._episode_end_times) > 0:
-                self._time_offset -= self._episode_end_times[-1]
-        if self._total_steps is None:
-            self._total_steps = sum(self._episode_lengths)
-        # update monitor state
-        if self._current_reward is not None:
-            self._episode_rewards.append(self._current_reward)
-            self._episode_lengths.append(self._num_steps)
-            self._episode_end_times.append(time.time() - self._time_offset)
-        # reset episode state
-        self._current_reward = 0
-        self._num_steps = 0
-
-        return obs
-
-    def _step(self, action):
-        obs, rew, done, info = self.env.step(action)
-        self._current_reward += rew
-        self._num_steps += 1
-        self._total_steps += 1
-        info['steps'] = self._total_steps
-        info['rewards'] = self._episode_rewards
-        return (obs, rew, done, info)
-
-    def get_state(self):
-        return {
-            'env_id': self.env.unwrapped.spec.id,
-            'episode_data': {
-                'episode_rewards': self._episode_rewards,
-                'episode_lengths': self._episode_lengths,
-                'episode_end_times': self._episode_end_times,
-                'initial_reset_time': 0,
-            }
-        }
-
-    def set_state(self, state):
-        assert state['env_id'] == self.env.unwrapped.spec.id
-        ed = state['episode_data']
-        self._episode_rewards = ed['episode_rewards']
-        self._episode_lengths = ed['episode_lengths']
-        self._episode_end_times = ed['episode_end_times']
+#     def __float__(self):
+#         """Get the current estimate"""
+#         return self._value
 
 
-def boolean_flag(parser, name, default=False, help=None):
-    """Add a boolean flag to argparse parser.
+# class SimpleMonitor(gym.Wrapper):
+#     def __init__(self, env):
+#         """Adds two qunatities to info returned by every step:
 
-    Parameters
-    ----------
-    parser: argparse.Parser
-        parser to add the flag to
-    name: str
-        --<name> will enable the flag, while --no-<name> will disable it
-    default: bool or None
-        default value of the flag
-    help: str
-        help string for the flag
-    """
-    dest = name.replace('-', '_')
-    parser.add_argument("--" + name, action="store_true", default=default, dest=dest, help=help)
-    parser.add_argument("--no-" + name, action="store_false", dest=dest)
+#             num_steps: int
+#                 Number of steps takes so far
+#             rewards: [float]
+#                 All the cumulative rewards for the episodes completed so far.
+#         """
+#         super().__init__(env)
+#         # current episode state
+#         self._current_reward = None
+#         self._num_steps = None
+#         # temporary monitor state that we do not save
+#         self._time_offset = None
+#         self._total_steps = None
+#         # monitor state
+#         self._episode_rewards = []
+#         self._episode_lengths = []
+#         self._episode_end_times = []
+
+#     def _reset(self):
+#         obs = self.env.reset()
+#         # recompute temporary state if needed
+#         if self._time_offset is None:
+#             self._time_offset = time.time()
+#             if len(self._episode_end_times) > 0:
+#                 self._time_offset -= self._episode_end_times[-1]
+#         if self._total_steps is None:
+#             self._total_steps = sum(self._episode_lengths)
+#         # update monitor state
+#         if self._current_reward is not None:
+#             self._episode_rewards.append(self._current_reward)
+#             self._episode_lengths.append(self._num_steps)
+#             self._episode_end_times.append(time.time() - self._time_offset)
+#         # reset episode state
+#         self._current_reward = 0
+#         self._num_steps = 0
+
+#         return obs
+
+#     def _step(self, action):
+#         obs, rew, done, info = self.env.step(action)
+#         self._current_reward += rew
+#         self._num_steps += 1
+#         self._total_steps += 1
+#         info['steps'] = self._total_steps
+#         info['rewards'] = self._episode_rewards
+#         return (obs, rew, done, info)
+
+#     def get_state(self):
+#         return {
+#             'env_id': self.env.unwrapped.spec.id,
+#             'episode_data': {
+#                 'episode_rewards': self._episode_rewards,
+#                 'episode_lengths': self._episode_lengths,
+#                 'episode_end_times': self._episode_end_times,
+#                 'initial_reset_time': 0,
+#             }
+#         }
+
+#     def set_state(self, state):
+#         assert state['env_id'] == self.env.unwrapped.spec.id
+#         ed = state['episode_data']
+#         self._episode_rewards = ed['episode_rewards']
+#         self._episode_lengths = ed['episode_lengths']
+#         self._episode_end_times = ed['episode_end_times']
 
 
-def get_wrapper_by_name(env, classname):
-    """Given an a gym environment possibly wrapped multiple times, returns a wrapper
-    of class named classname or raises ValueError if no such wrapper was applied
+# def boolean_flag(parser, name, default=False, help=None):
+#     """Add a boolean flag to argparse parser.
 
-    Parameters
-    ----------
-    env: gym.Env of gym.Wrapper
-        gym environment
-    classname: str
-        name of the wrapper
+#     Parameters
+#     ----------
+#     parser: argparse.Parser
+#         parser to add the flag to
+#     name: str
+#         --<name> will enable the flag, while --no-<name> will disable it
+#     default: bool or None
+#         default value of the flag
+#     help: str
+#         help string for the flag
+#     """
+#     dest = name.replace('-', '_')
+#     parser.add_argument("--" + name, action="store_true", default=default, dest=dest, help=help)
+#     parser.add_argument("--no-" + name, action="store_false", dest=dest)
 
-    Returns
-    -------
-    wrapper: gym.Wrapper
-        wrapper named classname
-    """
-    currentenv = env
-    while True:
-        if classname == currentenv.class_name():
-            return currentenv
-        elif isinstance(currentenv, gym.Wrapper):
-            currentenv = currentenv.env
-        else:
-            raise ValueError("Couldn't find wrapper named %s" % classname)
+
+# def get_wrapper_by_name(env, classname):
+#     """Given an a gym environment possibly wrapped multiple times, returns a wrapper
+#     of class named classname or raises ValueError if no such wrapper was applied
+
+#     Parameters
+#     ----------
+#     env: gym.Env of gym.Wrapper
+#         gym environment
+#     classname: str
+#         name of the wrapper
+
+#     Returns
+#     -------
+#     wrapper: gym.Wrapper
+#         wrapper named classname
+#     """
+#     currentenv = env
+#     while True:
+#         if classname == currentenv.class_name():
+#             return currentenv
+#         elif isinstance(currentenv, gym.Wrapper):
+#             currentenv = currentenv.env
+#         else:
+#             raise ValueError("Couldn't find wrapper named %s" % classname)
 
 
 def relatively_safe_pickle_dump(obj, path, compression=False):
